de6b355cd9fca5568b8fbb2de226cd75ff32908d
Shubh Jangam <shubh.jangam@chitran.in>
2022-08-06 09:51:08 +0530

    Base Tables - Countries Module Done

---
+++ routes/base_tables/country.js
@@ -0,0 +1,12 @@
+const country = require("../../controllers/base_tables/country");
+
+const router = require("express").Router();
+
+router.get("/", country.index);
+router.post("/", country.create);
+router.get("/:id/edit",country.edit);
+router.put("/:id",country.update);
+router.delete("/:id",country.delete);
+router.get("/:id",country.show);
+
+module.exports = router;
